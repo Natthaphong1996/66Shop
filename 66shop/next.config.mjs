0834ff402bd9838a1d4d3fc0db228223c07131c1
Natthaphong1996 <nattaphong.meesawad@gmail.com>
2024-04-27 23:18:31 +0700

    เพิ่มปุ่ม สั่งซื้อ และ รายละเอียด

--- 66shop/next.config.mjs
+++ 66shop/next.config.mjs
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ["www.gamesrig.com", "steamspy.com","raw.githubusercontent.com","steamcdn-a.akamaihd.net"],
+        domains: ["www.gamesrig.com", 
+        "steamspy.com",
+        "raw.githubusercontent.com",
+        "steamcdn-a.akamaihd.net",
+        "cdn.akamai.steamstatic.com"
+    ],
     }
 };
 export default nextConfig;
--- 66shop/src/app/components/GamesData.jsx
+++ 66shop/src/app/components/GamesData.jsx
@@ -23,33 +23,42 @@ function GamesData() {
             ) : (
                 <div className='grid grid-cols-5 content-center'>
                     {game.map((val, index) => (
-                            <div className='flex flex-col justify-center items-center shadow-md transition cursor-pointer hover:shadow-lg m-2 rounded-md '>
-                                <div className='m-2 line-clamp-1'>
-                                    <h4>{val.name}</h4>
-                                </div>
-                                <div className='mb-2 relative flex flex-row-reverse text-white transition ease-in-out delay-50 hover:-translate-y-1 hover:scale-110 duration-300'>
-                                    
-                                        <Image src={val.img} width={150} height={150} alt={val.name} />
-                                        {val.discount > 0 && (
-                                            <span className='bg-red-600 shadow-md text-white font-quantico text-sm px-3 py-2 absolute top-0 right-0 z-50'>{val.discount}%</span>
-                                        )}
-                                    
-                                </div>
-                                <div className='mb-2'>
-                                    {val.discount > 0 ? (
-                                        <div>
-                                            <div><price>ราคาเต็ม {val.price} บาท</price></div>
-                                            <div><discount>ลด {val.discount} % เหลือ {val.price - ((val.price / 100) * val.discount)} บาท</discount></div>
-                                        </div>
-                                    ) : (
-                                        <div>
-                                            <div><price>ราคา {val.price} บาท</price></div>
-                                            <br />
-                                        </div>
-
-                                    )}
-                                </div>
+                        <div className='flex flex-col justify-center items-center shadow-md transition cursor-pointer hover:shadow-lg m-2 rounded-md'>
+                            <div className='m-2 line-clamp-1'>
+                                <h4>{val.name}</h4>
                             </div>
+                            <div className='mb-2 relative flex flex-row-reverse text-white transition ease-in-out delay-50 hover:-translate-y-1 hover:scale-110 duration-300'>
+                                <Image src={val.img} width={150} height={150} alt={val.name} />
+                                {val.discount > 0 && (
+                                    <span className='bg-red-600 shadow-md text-white font-quantico text-sm px-3 py-2 absolute top-0 right-0 z-50'>{val.discount}%</span>
+                                )}
+                            </div>
+                            <div className='mb-2'>
+                                {val.discount > 0 ? (
+                                    <div>
+                                        <div><price>ราคาเต็ม {val.price} บาท</price></div>
+                                        <div><discount>ลด {val.discount} % เหลือ {val.price - ((val.price / 100) * val.discount)} บาท</discount></div>
+                                    </div>
+                                ) : (
+                                    <div>
+                                        <div><price>ราคา {val.price} บาท</price></div>
+                                        <br />
+                                    </div>
+                                )}
+                                <script async
+                                    src="https://js.stripe.com/v3/buy-button.js">
+                                </script>
+
+                                <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
+                                    สั่งซื้อ
+                                </button>
+
+                                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded  ml-2">
+                                    รายละเอียด
+                                </button>
+                            </div>
+                        </div>
+
                     ))}
                 </div>
             )}
